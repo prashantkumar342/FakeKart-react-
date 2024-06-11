9c92f59da95ee396f0f448299545167e768698b3
prashantkumar342 <tuhaniaji@gmail.com>
2024-06-11 15:58:41 +0530

    Home page UI modified

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,67 +8,60 @@ const Home = () => {
     $('.navMenuLinks').removeClass('active')
   })
   const [categoryBtns, setCategoryBtns] = useState([])
-  const [sliderImages, setSliderImages] = useState([])
-  // const [saleSpecialProducts, setSaleSpecialProducts] = useState([])
+  const [hotproducts, SetHotProducts] = useState([]);
   useEffect(() => {
     axios.get('https://fakestoreapi.in/api/products/category')
       .then(data => setCategoryBtns(data.data.categories))
   }, [])
-
   useEffect(() => {
-    axios.get('https://fakestoreapi.in/api/products?limit=20')
-      .then(data => setSliderImages(data.data.products))
+    axios.get('https://fakestoreapi.in/api/products?limit=4')
+      .then(response => SetHotProducts(response.data.products))
   }, [])
-  // useEffect(() => {
-  //   axios.get('https://fakestoreapi.in/api/products?limit=4')
-  //     .then(data => setSaleSpecialProducts(data.data.products))
-  // }, [])
-  // const addToCart = (productId) => {
-  //   let cart = JSON.parse(localStorage.getItem('cartItems')) || []
-  //   let found = cart.some(id => id == productId)
-  //   if (!found) {
-  //     cart.push(productId)
-  //   }
-  //   localStorage.setItem('cartItems', JSON.stringify(cart))
-  // }
+
   return (
     <>
       <ul className="categoryBtnWrapper">
         {categoryBtns.map((item, index) =>
           <li key={index} className='categoryBtn' id={item}>
-            <Link to={`/${item}`}>
-              <i className="fa-solid fa-hashtag">
-              </i> {item}</Link></li>
+            <Link to={`/${item}`}>{item}</Link></li>
         )}
       </ul>
-      <div className="overview">
-        <div className="slideImages">
-          <p onClick={() => { $('.categoryBtnWrapper').toggleClass('active'); $('.navMenuLinks').removeClass('active') }}><i className="fa-solid fa-bars-progress" ></i></p>
-          {sliderImages.map((product, index) => (
-            <img key={index}
-              className='sliderImg'
-              src={product.image}
-            />
-          ))}
+      <div className="saleOverview">
+        <div className="salephoto">
+          <img src="../public/sale.png" alt="" />
         </div>
-        <div className="salecard">
-          <div className="saleaside">
-            <p className="saleName">Bigest fake sale of the year <i className="fa-solid fa-bell"></i></p>
-            <p className="des" >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi error voluptatibus omnis et atque. Eos, aliquam, nam dicta praesentium ducimus omnis modi tenetur magnam, rem voluptatum ipsum! Eveniet quidem voluptatum amet nesciunt mollitia natus deserunt eligendi maxime, numquam explicabo rem.</p>
-            <button className="btn"><i className="fa-solid fa-shop"></i> Go To ShowRoom <i className="fa-solid fa-arrow-up-right-from-square"></i></button>
-            <p className="brands">
-              HOT BRANDS</p>
-            <div className="brandsBtns">
-              <button className="brandbtn" id="apple">Apple <i className="fa-solid fa-arrow-up-right-from-square"></i></button>
-              <button className="brandbtn" id="samsung">Samsung <i className="fa-solid fa-arrow-up-right-from-square"></i></button>
-              <button className="brandbtn" id="boat">Boat <i className="fa-solid fa-arrow-up-right-from-square"></i></button>
-              <button className="brandbtn" id="microsoft">Microsoft <i className="fa-solid fa-arrow-up-right-from-square"></i></button>
-            </div>
+        <div className="hotsale">
+          <div className="hotproducts">
+            {
+              hotproducts.map((product) => (
+                <div className="card" key={product.id}>
+                  <div className="image">
+                    <img src={product.image} alt="" />
+                  </div>
+                  <p className="title">{product.title.slice(0, 10)}...</p>
+                  <div className="btns">
+                    <button className="addtocart"><i className="fa-solid fa-cart-plus"></i></button>
+                    <button className="addtowishlist"><i className="fa-solid fa-heart-circle-plus"></i></button>
+
+                  </div>
+
+                </div>
+              ))
+            }
+
+          </div>
+          <div className="hotbrands">
+            <button>Apple <i className="fa-solid fa-arrow-up-right-from-square"></i></button>
+            <button>MicroSoft <i className="fa-solid fa-arrow-up-right-from-square"></i></button>
+            <button>Samsung <i className="fa-solid fa-arrow-up-right-from-square"></i></button>
+            <button>Boat <i className="fa-solid fa-arrow-up-right-from-square"></i></button>
+          </div>
+          <div className="saledetails">
+            <p className="salename">Biggest Fake Sale Of The Year</p>
+            <p className="saledes">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi deserunt, minus corporis mollitia, assumenda iure modi doloremque, iste commodi aliquam ipsam? Eius nisi quisquam, voluptatum eum sequi assumenda dolorem itaque!</p>
           </div>
         </div>
-      </div >
-
-
+      </div>
     </>
   );
 }
